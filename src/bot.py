d51ebb85e6fdc9255dbf1f7cc4aa7e18e052e7a5
flyb1z0n <flyb1z0n@gmail.com>
2024-02-17 21:14:07 -0800

    added user filtering

--- src/bot.py
+++ src/bot.py
@@ -3,6 +3,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler
 from dotenv import load_dotenv
 import os
+from config import FlYB1Z0N_USER_ID
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -13,10 +14,14 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 async def onMessage(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Only allow messages from the allowed user
+    if update.effective_user.id != FlYB1Z0N_USER_ID:
+        logging.info("Not allowed user: " + str(update.effective_user.id) + " - " + update.message.text) 
+        return
     await context.bot.send_message(chat_id=update.effective_chat.id, text="RESP: " + update.message.text)
 
 def main():
-    load_dotenv()
+    load_dotenv(override=True)
     botToken = os.getenv('TELEGRAM_BOT_TOKEN')
     application = ApplicationBuilder().token(botToken).build()
     
