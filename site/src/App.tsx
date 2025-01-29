0e9abf3b3f60d248f5111b5e0943be548760b83a
flyb1z0n <flyb1z0n@gmail.com>
2025-01-28 21:21:19 -0800

    First tg mini app

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,22 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from './Button'
+import { useState } from "react";
+
+import "./App.css";
+import { Button } from "./Button";
+import { retrieveLaunchParams } from "@telegram-apps/sdk";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const { tgWebAppData } = retrieveLaunchParams();
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <div>{JSON.stringify(tgWebAppData)}</div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -32,7 +27,7 @@ function App() {
 
       <Button />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
